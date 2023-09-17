a4aac9d9ddad4c0c0d306181070b759100e2cf85
R0LEX& <himanshugola1111@gmail.com>
2023-09-17 22:19:58 +0530

    Memoization & Tabulation

--- Dynamic Programming/fibonacci.cpp
+++ Dynamic Programming/fibonacci.cpp
@@ -2,6 +2,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/* memoization
+
 int fib(int n , vector<int>& dp){
     // base case
     if(n < 2) return n;
@@ -10,16 +12,30 @@ int fib(int n , vector<int>& dp){
 
     return dp[n] = fib(n-1 , dp) + fib(n-2 , dp); 
 }
+*/
 
 
 int main(){
     int n ;
     cout<<"Enter n :"<<endl;
     cin >> n;
-    vector<int> dp(n+1 , -1);
+    // vector<int> dp(n+1 , -1);
 
-    int ans = fib(n, dp);
-    cout<<ans<<endl;
+    // int ans = fib(n, dp);
+    // cout<<ans<<endl;
+
+    int prev2 = 0 ;
+    int prev = 1;
+    int curr = 0 ;
+
+    for(int i = 2; i <= n; i++){
+
+        curr = prev + prev2;
+        prev2 = prev;
+        prev = curr;
+    }
+
+    cout<<curr<<endl;
 
     return 0;
 }

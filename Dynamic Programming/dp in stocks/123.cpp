40051d1b5c10b23e41be71624e408191e722346e
Himanshu Kumar Gola <himanshugola1111@gmail.com>
2024-05-10 23:31:37 +0530

    123 LC ✅

---
+++ Dynamic Programming/dp in stocks/123.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+int f(int i , int buy , int cap, vector<int>& arr , vector<vector<vector<int>>>& dp){
+    // base case
+    if(i == arr.size() || cap == 0 ) return 0;
+
+    // dp check
+    if(dp[i][buy][cap] != -1) {
+
+        return  dp[i][buy][cap];
+    };
+
+    if(buy){
+        return dp[i][buy][cap] = max(-arr[i] + f(i+1 , 0, cap  , arr , dp) ,f(i+1 , 1 , cap , arr  , dp));
+    }else {
+        int newCap = cap-1;
+        return dp[i][buy][cap] =  max(arr[i] + f(i+1 , 1 , newCap, arr , dp) ,f(i+1 , 0 , cap, arr, dp));
+    }
+}
+public:
+    int maxProfit(vector<int>& prices) {
+        int n= prices.size();
+         vector<vector<vector<int>>> dp(n , vector<vector<int>>(2 , vector<int>(3,-1)));
+        return f(0 , 1 , 2 , prices , dp);
+    }
+};
